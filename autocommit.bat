836d86d573f0799d4e866fc215ed8346f79014fe
Rishu Sharma <rishushar99@gmail.com>
2023-09-20 15:11:59 -0500

    update for older flutter versions

---
+++ autocommit.bat
@@ -0,0 +1,5 @@
+E:
+cd "E:\Users\rishu\OneDrive\Documents\GitHub\cringe_miner"
+git add -u
+git commit -m "Auto Commit"
+git push origin auto-save
--- lib/components/item.dart
+++ lib/components/item.dart
@@ -25,7 +25,7 @@ class _ItemWidgetState extends State<ItemWidget> {
       children: [
         SizedBox(
             height: 60,
-            width: MediaQuery.sizeOf(context).width,
+            width: MediaQuery.of(context).size.width,
             child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                 child: ClipRRect(
@@ -37,15 +37,15 @@ class _ItemWidgetState extends State<ItemWidget> {
                               ? CupertinoColors.darkBackgroundGray
                               : CupertinoColors.lightBackgroundGray,
                           height: 40,
-                          width: MediaQuery.sizeOf(context).width - 40),
+                          width: MediaQuery.of(context).size.width - 40),
                       AnimatedSize(
                         duration: Duration(milliseconds: 1000 ~/ 60),
                         child: ClipRRect(
                             borderRadius: BorderRadius.circular(10),
                             child: Container(
                                 color: CupertinoColors.activeBlue,
-                                height: 40,
-                                width: (MediaQuery.sizeOf(context).width - 40) *
+                                height: 40,Auto Commit
+                                width: (MediaQuery.of(context).size.width - 40) *
                                     (widget.timeElapsed / widget.totalTime))),
                       )
                     ],
