9f24f8a458c928ce200079b7e4ca382287f18988
Rishu Sharma <rishushar99@gmail.com>
2023-09-20 19:53:18 -0500

    Auto Commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,12 @@
 Map<String, List<Map<String, dynamic>>> items = {
   'ancientEgypt': [
-    {'name': 'Sand', 'initialTime': 72, 'initialTimeInSeconds': 4, 'initialPrice': 3.738, 'coefficient': 1.06, }
+    {
+      'name': 'Sand',
+      'initialTime': 72,
+      'initialTimeInSeconds': 0.6,
+      'initialPrice': 3.738,
+      'coefficient': 1.06,
+      'initialRevenue': 1
+    }
   ]
 };
