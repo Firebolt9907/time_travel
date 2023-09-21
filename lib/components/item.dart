556dcd043715589e6ebdd76bcd3490e879617120
Rishu Sharma <rishushar99@gmail.com>
2023-09-20 19:32:15 -0500

    Auto Commit

--- lib/components/item.dart
+++ lib/components/item.dart
@@ -55,7 +55,7 @@ class _ItemWidgetState extends State<ItemWidget> {
                             return Container(
                               color: CupertinoColors.activeBlue,
                               height: 40,
-                              width: time.value % widget.totalTime == 0 || time.value % widget.totalTime(MediaQuery.of(context).size.width - 40) *
+                              width: (MediaQuery.of(context).size.width - 40) *
                                   (time.value % widget.totalTime) /
                                   widget.totalTime,
                             );
