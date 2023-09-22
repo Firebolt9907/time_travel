8f2a401d771e6d9d21173f842fab72ec978555fb
Rishu Sharma <83421723+Firebolt9907@users.noreply.github.com>
2023-09-22 09:13:05 -0500

    added more items bc im bored after ush test

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,15 +2,88 @@ Map<String, List<Map<String, dynamic>>> items = {
   'ancientEgypt': [
     {
       'name': 'Money',
+      'timePeriod': '2600 BC - 30 BC'
+      'location': 'Ancient Egypt'
     },
     {
-      'name': 'Sand',
+      'name': 'Ancient Sand',
       'initialTime': 72,
       'initialTimeInSeconds': 0.6,
       'initialPrice': 3.738,
       'coefficient': 1.07,
       'initialRevenue': 1,
       'upgradeLevel': 1,
-    }
+      'description': 'You know what sand is...'
+    },
+    {
+      'name': 'Canopic Jars',
+      'initialTime': 72,
+      'initialTimeInSeconds': 0.6,
+      'initialPrice': 3.738,
+      'coefficient': 1.07,
+      'initialRevenue': 1,
+      'upgradeLevel': 1,
+      'description': 'Canopic jars were used in ancient Egypt to hold the preserved organs of mummies. They were often adorned with depictions of the four sons of Horus.'
+    },
+    {
+      'name': 'Hieroglyphic Tablets',
+      'initialTime': 72,
+      'initialTimeInSeconds': 0.6,
+      'initialPrice': 3.738,
+      'coefficient': 1.07,
+      'initialRevenue': 1,
+      'upgradeLevel': 1,
+      'description': 'Hieroglyphic tablets were inscribed with intricate pictorial writing, telling stories and conveying important information in ancient Egypt.'
+    },
+    {
+      'name': 'Sacred Oils',
+      'initialTime': 72,
+      'initialTimeInSeconds': 0.6,
+      'initialPrice': 3.738,
+      'coefficient': 1.07,
+      'initialRevenue': 1,
+      'upgradeLevel': 1,
+      'description': 'Ancient Egyptians used various oils and incense for religious rituals and healing purposes. They were often derived from rare herbs and plants.'
+    },
+    {
+      'name': 'Papyrus Scrolls',
+      'initialTime': 72,
+      'initialTimeInSeconds': 0.6,
+      'initialPrice': 3.738,
+      'coefficient': 1.07,
+      'initialRevenue': 1,
+      'upgradeLevel': 1,
+      'description': 'Papyrus scrolls were ancient Egyptian documents made from the papyrus plant. They contained a wide range of knowledge, including religious texts, historical records, and medical treatises.'
+    },
+    {
+      'name': 'Scarab Amulets',
+      'initialTime': 72,
+      'initialTimeInSeconds': 0.6,
+      'initialPrice': 3.738,
+      'coefficient': 1.07,
+      'initialRevenue': 1,
+      'upgradeLevel': 1,
+      'description': 'Scarab amulets were small, beetle-shaped artifacts made from various materials such as stone, faience, or precious metals. They were believed to bring protection, good luck, and ward off evil spirits in ancient Egypt.'
+    },
+    {
+      'name': "Pharoah's Treasures",
+      'initialTime': 72,
+      'initialTimeInSeconds': 0.6,
+      'initialPrice': 3.738,
+      'coefficient': 1.07,
+      'initialRevenue': 1,
+      'upgradeLevel': 1,
+      'description': "Pharaoh's treasures refer to the priceless artifacts, jewelry, and royal relics found in the tombs of Egyptian pharaohs, such as Tutankhamun's tomb."
+    },
+    {
+      'name': 'da piyramids',
+      'initialTime': 72,
+      'initialTimeInSeconds': 0.6,
+      'initialPrice': 3.738,
+      'coefficient': 1.07,
+      'initialRevenue': 1,
+      'upgradeLevel': 1,
+      'description': 'da scrumdiddliumptious piyramids of egipt'
+    },
   ]
 };
